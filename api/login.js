ca965c4ebfed2d399668b20a90ec8912981bd010
Miyoso <miyasajeux@gmail.com>
2025-11-07 09:55:19 +0100

    Ada

--- api/login.js
+++ api/login.js
@@ -1,6 +1,14 @@
 import { sql } from '@vercel/postgres';
 import bcrypt from 'bcrypt';
 
+
+const ADA_USERNAME = 'PROTO';
+
+
+const ADA_PASSWORD_HASH = '$2a$12$IzPCtcn80IDrHEHPOpDBiOnw8qK/Nz/SrZJU0mXcbzpgk3oi71ZfK'; 
+const ADA_LEVEL = 7;
+// ----------------------------------------------
+
 export default async function handler(request, response) {
   if (request.method !== 'POST') {
     return response.status(405).json({ success: false, message: 'Method Not Allowed' });
@@ -13,6 +21,23 @@ export default async function handler(request, response) {
       return response.status(400).json({ success: false, message: 'AGENT_ID et PASSPHRASE requis' });
     }
 
+    
+    if (username === ADA_USERNAME) {
+        const isAdaPasswordValid = await bcrypt.compare(password, ADA_PASSWORD_HASH);
+        
+        if (isAdaPasswordValid) {
+             return response.status(200).json({
+                success: true,
+                user: ADA_USERNAME,
+                level: ADA_LEVEL
+            });
+        } else {
+            
+             return response.status(401).json({ success: false, message: '//: ERROR :: PASSPHRASE_INCORRECT' });
+        }
+    }
+
+  
     const { rows } = await sql`
         SELECT id, name, level, password 
         FROM agents 
@@ -24,8 +49,6 @@ export default async function handler(request, response) {
     }
 
     const agent = rows[0];
-
-    
     const isPasswordValid = await bcrypt.compare(password, agent.password);
 
     if (isPasswordValid) {
