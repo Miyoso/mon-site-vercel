17f730dc23dc155acbed0fc332ab91019ed45e91
miyoso <miyasajeux@gmail.com>
2025-11-09 16:40:19 +0100

    map

--- api/get-agents.js
+++ api/get-agents.js
@@ -1,28 +1,33 @@
+
 import { sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
+
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
   try {
-    const { rows: agents } = await sql`
-      SELECT 
-        id, 
-        name, 
-        status_text, 
-        level, 
-        map_x, 
-        map_y, 
-        profile_url, 
-        css_class, 
-        warning_level
-      FROM 
-        agents
-      WHERE 
-        map_x IS NOT NULL AND map_y IS NOT NULL;
+   
+    const { rows } = await sql`
+      SELECT id, name, level, status_text, map_x, map_y 
+      FROM agents 
+      WHERE map_x IS NOT NULL AND map_y IS NOT NULL;
     `;
 
-    return response.status(200).json({ agents });
+    return response.status(200).json({ agents: rows });
 
   } catch (error) {
-    console.error("Erreur lors de la récupération des agents:", error);
-    return response.status(500).json({ error: "Échec de la récupération des données de la base de données." });
+    console.error('Database Error:', error);
+    return response.status(500).json({ error: 'Erreur lors de la récupération des agents.' });
   }
 }
