cf11b7e649bef3938ebc2668671e59977754a4c8
miyoso <miyasajeux@gmail.com>
2025-11-15 23:55:05 +0100

    Update security.js

--- public/js/security.js
+++ public/js/security.js
@@ -5,9 +5,9 @@ const userLevel = parseInt(localStorage.getItem('sas_level') || 0);
 if (typeof REQUIRED_LEVEL === 'undefined') {
     
     console.error("ERREUR SYSTÈME: NIVEAU DE SÉCURITÉ NON DÉFINI dans la page.");
-    window.location.href = 'access-denied.html'; 
+    window.location.href = '/access-denied.html'; 
 } else if (userLevel < REQUIRED_LEVEL) {
     
-    window.location.href = 'access-denied.html'; 
+    window.location.href = '/access-denied.html'; 
 }
 
